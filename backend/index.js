0d9c8a2551d7138071f86cb2aaf30f218dbef014
Washington Gomes <wasrepet@gmail.com>
2020-12-02 22:48:20 -0300

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.get ("/", function(request, response){
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.get ("/video", function(request, response){
+    const range = request.headers.range;
+    if(!range){
+        response.status(400).send("Requires Range Header");
+    }
+
+    const videoPath = "./videos/Raindrops_240p.mp4";
+    const videoSize = fs.statSync("./videos/Raindrops_240p.mp4").size;
+
+    // Parse Range
+    //Example = "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "videos/mp4",
+    };
+    response.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    videoStream.pipe(response);
+});
+
+app.listen(3000, function(){
+    console.log("Listen port 3000!");
+})
