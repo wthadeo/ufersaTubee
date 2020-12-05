418544bd4dfe8edd4d78957d1837b7a23d4c495e
Washington <wthadeo@users.noreply.github.com>
2020-12-05 15:57:43 -0300

    segundo commit
    habilitada a troca de resoluçoes dos videos

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,19 @@ app.get ("/", function(request, response){
     response.sendFile(__dirname + "/index.html");
 });
 
-app.get ("/video", function(request, response){
+
+app.get ("/video/:video", function(request, response){
     const range = request.headers.range;
+
+    const uri = "./videos/" + request.params.video;
+    
+
     if(!range){
         response.status(400).send("Requires Range Header");
     }
 
-    const videoPath = "./videos/Raindrops_240p.mp4";
-    const videoSize = fs.statSync("./videos/Raindrops_240p.mp4").size;
+    const videoPath = uri;
+    const videoSize = fs.statSync(uri).size;
 
     // Parse Range
     //Example = "bytes=32324-"
